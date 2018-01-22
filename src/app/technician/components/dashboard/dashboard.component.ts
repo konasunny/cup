6af9d305f9af441a3f08dea80fcd5e88d624740b
sandeep kona <sandeep.kona@caprusit.com>
2018-01-22 15:14:20 +0530

    added car icon

--- src/app/technician/components/dashboard/dashboard.component.ts
+++ src/app/technician/components/dashboard/dashboard.component.ts
@@ -36,6 +36,18 @@ destLocation = 'pragathi nagar';
     endLocation = [];
     polyLine = [];
     poly2 = [];
+    car = "M17.402,0H5.643C2.526,0,0,3.467,0,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644 V6.584C23.044,3.467,20.518,0,17.402,0z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z";
+   icon = {
+    path: this.car,
+    scale: .7,
+    strokeColor: 'white',
+    strokeWeight: .10,
+    fillOpacity: 1,
+    fillColor: '#404040',
+    offset: '5%',
+    // rotation: parseInt(heading[i]),
+    anchor: new google.maps.Point(10, 25) // orig 10,50 back of car, 10,0 front of car, 10,25 center of car
+};
   styledMapType = new google.maps.StyledMapType(
     [
       {
@@ -135,7 +147,7 @@ destLocation = 'pragathi nagar';
 
     this.geoLocation.getCurrentLocation({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true,  frequency: 3000 }).subscribe((resp) => {
       mylocation = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-      let image = 'assets/images/location-tracker.png';
+      let image = this.icon; //'assets/images/location-tracker.png';
 
       this.initializeMap(mylocation);
       this.styledMap()
@@ -233,6 +245,7 @@ destLocation = 'pragathi nagar';
       this.onWatchSuccess(data)
     });
   }
+
 
   onWatchSuccess(data) {
     //this.deleteMarkers();
